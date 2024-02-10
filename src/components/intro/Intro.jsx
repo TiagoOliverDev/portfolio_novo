bd5dd0fd798946203181d191cb5e07317b8bb839
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-10 15:01:02 -0300

    feat: intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,8 +7,8 @@ import './intro.css';
 const Intro = () => {
   return (
     <section id="about">
-      <h5>Get to know</h5>
-      <h2>About Me</h2>
+      <h5>Conheça</h5>
+      <h2>Sobre mim</h2>
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
